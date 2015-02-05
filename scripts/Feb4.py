99897e3c9dc86cea01fb17773e8b9f3e9508a5d0
Vanessa Apkenas <vanessaapkenas@ucla.edu>
2015-02-05 04:02:13 +0000

    This is my homework from python!

---
+++ scripts/Feb4.py
@@ -0,0 +1,14 @@
+from __future__ import division
+
+#! usr/bin/python
+
+def get_at_content(dna):
+    length = len(dna)
+    a_count = dna.upper().count('A')
+    t_count = dna.upper().count('T')
+    at_content = (a_count + t_count) / length
+    return round(at_content, 2)
+my_at_content = get_at_content("ATGCGCGATCGATCGAATCG")
+print(str(my_at_content))
+print(get_at_content("ATGCATGCAACTGTAGC"))
+print(get_at_content("aactgtagctagctagcagcgta"))
